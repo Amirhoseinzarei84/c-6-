2044d127ded3824d8f0944c96c7f44e8e613a40f
amirhoseinzarei <amirhoseinzarei8484@gmail.com>
2024-11-04 23:55:30 +0330

    Add files via upload
    تمرین 6 درخت تصمیم

---
+++ تمرین 7 شماره تلفن با سی شارپ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("please enter a or b ");
+char input;
+input =Console.ReadKey().KeyChar;
+
+if (input == 'a' || input == 'A')
+
+    if (input == 'r' || input == 'B') ;
+
+
+    else if (input == 'b' || input == 'B') ;
